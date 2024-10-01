e6e5bfc512351607dbf47ad20b0b311c801c50dd
Sagar Vishwakarma <sv472921@gmail.com>
2024-10-01 14:53:25 +0530

    commit

--- Server/Server.js
+++ Server/Server.js
@@ -4,7 +4,6 @@ import http from 'http';
 import bodyParser from 'body-parser';
 import { Server} from 'socket.io'
 import mongoConnect from './db/MongoDB.js';
-import path from 'path';
 import { handleRegister, handleLogin, handleLogout, getUser, getUserConversation, addUserChat, getUserChat , otherUserAdd , changeProfilePic } from './Controller/userController.js';
 import { changeUserProfile , getUserProfile} from './Controller/profileController.js'
 import { validateSchema, loginValidation } from './Validations/registerValidation.js';
@@ -108,7 +107,7 @@ io.on('connection' , (socket)=>{
 
 // server listen
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 6000;
 server.listen(port, () => {
   console.log(`server listening on ${port} `)
 })

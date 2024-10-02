8c82ecd0d095bfe67d561bb5354caa0dd6694a66
Sagar Vishwakarma <sv472921@gmail.com>
2024-10-02 12:23:49 +0530

    commit

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/user': 'http://localhost:8080',
+      '/user': process.env.URL,
     },
   },
   plugins: [react()],

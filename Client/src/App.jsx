d9abc971d63d8226d280ab38293d84010d3f6fae
Sagar Vishwakarma <sv472921@gmail.com>
2024-10-02 14:14:38 +0530

    commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,25 +1,23 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from './Pages/Register.jsx'
-import Login from './Pages/Login.jsx';
-import Chat from './Components/ChatComponents/Chat.jsx';
-import Userprofile from './Pages/Userprofile.jsx';
-import AddFriend from './Components/ChatComponents/AddFriend.jsx';
+import Register from './Pages/Register'
+import Login from './Pages/Login';
+import Chat from './Components/ChatComponents/Chat';
+import Userprofile from './Pages/Userprofile';
+import AddFriend from './Components/ChatComponents/AddFriend';
 
 function App() {
 
-  return (
-    <>
+  return ( 
     <Router>
         <Routes>
-          <Route exact path="/" Component={<Chat/>} />
-          <Route exact path="/profile" Component={<><Userprofile/></>} />
-          <Route exact path="/register" Component={<Register/>} />
-          <Route exact path="/login" Component={<Login/>} />
-          <Route exact path="/adduser" Component={<AddFriend/>} />
+          <Route exact path="/" element={<Chat/>} />
+          <Route exact path="/profile" element={<><Userprofile/></>} />
+          <Route exact path="/register" element={<Register/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/adduser" element={<AddFriend/>} />
         </Routes>
     </Router>
-     </>
   )
 }
 
